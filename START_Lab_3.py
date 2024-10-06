1a287778b44c81e7bc772e1cc32ad4e9ec7d08db
renea-z <reneazattriqi@gmail.com>
2024-10-06 00:04:54 -0600

    just checking

--- START_Lab_3.py
+++ START_Lab_3.py
@@ -4,13 +4,25 @@ def lab3Question1(number, cutoff):
     # Return True if the number is less than the cutoff, False otherwise
     # Also, print a statement of "[Number] is less than [cutoff]" or "[Number] is not less than [cutoff]"
     # Where the [Number] and [cutoff] are the actual numbers passed in
-    pass
+   if number < cutoff:
+        print(f" {number} is less than {cutoff}")
+        return True
+   else:
+        print(f"{number} is not less than {cutoff}")
+        return False
 
 def lab3Question2(decimal_number):
     # Take in an argument of a float (decimal) number.
     # Return "zero" if the number is 0, "positive" if the number is positive, and "negative" if the number is negative
     # Return "invalid" if the input is not a float
-    pass
+    if decimal_number == 0:
+        return "zero"
+    elif not isinstance(decimal_number, float):
+       return "invalid"
+    elif decimal_number >0:
+        return "positive"
+    elif decimal_number <0:
+        return "negative"
 
 def lab3Question3(year):
     # Take in a number that represents a year
@@ -19,13 +31,30 @@ def lab3Question3(year):
     # "19th century" if the year is between 1801 and 1900, 
     # "ancient" if the year is older
     # "invalid" if the input is not an acceptable year number. 
-    pass
+    if not isinstance(year, int):
+        return "invalid"
+    elif 2001 <= year <= 2100:
+        return "21st century"
+    elif 1901 <= year <= 2000:
+        return "20th century"
+    elif 1801 <= year <= 1900:
+        return "19th century"
+    elif year < 1801:
+        return "ancient"
 
 def lab3Question4(number_1, number_2, number_3):
     # Take in three numbers as arguments
     # Return the largest of the three numbers
     # Return None if the inputs are not 3 numbers
-    pass
+    if not isinstance(number_1,(int,float)) or not isinstance(number_2, (int,float)) or not isinstance(number_3,(int,float)):
+        return None
+    elif number_1 > number_2 and number_1 > number_3:
+        largest_number = number_1
+    elif number_2 > number_1 and number_2 > number_3:
+        largest_number = number_2
+    else: 
+        largest_number = number_3
+    return largest_number
 
 def lab3Question5(temperature, scale_used):
     # Take in a temperature and the scale that the temperature is in - either "C" for Celsius or "F" for Fahrenheit (capitalized)
